709d62af6c3391783365fa08e0314c0dfd947896
karankasula <mr.kasulakaran@gmail.com>
2020-09-28 19:27:56 +0545

    data,methods,events,events modifier,data binding

--- vuejs-playlist/app.js
+++ vuejs-playlist/app.js
@@ -1,6 +1,21 @@
 var vue_det = new Vue({
     el: '#intro',
     data: {
-        message: 'This is my first veu js task'
+        name: 'karan',
+        website: 'https://www.facebook.com/fb.com',
+        age: 24
+    },
+    methods: {
+        greet: function(time) {
+            // this.name = 'muna'
+            return 'Good ' + time + ' ' + this.name;
+
+        },
+        logname: function() {
+            console.log("you entered your name");
+        },
+        logage: function() {
+            console.log("you entered your age");
+        }
     }
 });
